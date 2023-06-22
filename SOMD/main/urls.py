908ea335872fd439546fdec56416802c7c947fb5
JongbeomLee623 <jambon23@dgu.ac.kr>
2023-06-22 16:37:22 +0900

    feat:posting

--- SOMD/main/urls.py
+++ SOMD/main/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('mysomd/',mysomd,name="mysomd"),
     path('createSOMD/',createSOMD,name="createSOMD"),
     path('mainfeed/<int:id>',mainfeed,name="mainfeed"),
+    path('mainfeed/<int:somd_id>/new/',new, name="new"),
+    path('mainfeed/<int:somd_id>/createpost/',createpost,name="createpost"),
+    path('mainfeed/viewpost/<int:post_id>',viewpost,name="viewpost"),
 ]
