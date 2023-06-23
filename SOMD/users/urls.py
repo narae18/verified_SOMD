a8f4dd739017a8df159d19e073d9fa609e3a7288
shimseohyun <shim020924@gmail.com>
2023-06-23 19:54:17 +0900

    feat:mypage_update

--- SOMD/users/urls.py
+++ SOMD/users/urls.py
@@ -4,4 +4,6 @@ from .views import *
 app_name = "users"
 urlpatterns = [
     path("mypage/", mypage, name="mypage"),
+    path("mypage_edit/", mypage_edit, name="mypage_edit"),
+    path("mypage_update/", mypage_update, name="mypage_update"),
 ]
